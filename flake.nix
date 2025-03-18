bb889421886682dafd73114f7d3f4f54becf4d28
Henri Rosten <henri.rosten@unikie.com>
2025-03-18 13:42:14 +0200

    Initial commit
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Example public flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Example private repo
+    hello-private = {
+      url = "git+ssh://git@github.com/tiiuae/action-test-private";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    hello-private,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+    formatter = pkgs.writeShellScriptBin "format" ''
+      echo "[+] Running '$(realpath "$0")'"
+      if [[ $# = 0 ]]; then set -- .; fi
+      ${pkgs.lib.getExe pkgs.alejandra} "$@"
+    '';
+    say-hello = pkgs.writeShellScriptBin "say-hello" ''
+      set -eu
+      echo "[+] Running '$(realpath "$0")'"
+      echo "Hello from action-test-public!"
+    '';
+  in {
+    # packages
+    packages.x86_64-linux.hello = say-hello;
+    packages.x86_64-linux.hello-private = hello-private.packages.x86_64-linux.hello;
+
+    # formatter
+    formatter.x86_64-linux = formatter;
+  };
+}
